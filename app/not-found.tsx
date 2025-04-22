67fa21f73eb454297ebbb0c511f5f2e581570cb6
Lux <1171484646@qq.com>
2025-04-22 11:00:28 +0800

    Loading & Not Found Pages

---
+++ app/not-found.tsx
@@ -0,0 +1,27 @@
+import { Button } from '@/components/ui/button'
+import { APP_NAME } from '@/lib/contants'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const NotFoundPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <Image
+        src="/images/logo.svg"
+        width={48}
+        height={48}
+        alt={`${APP_NAME} logo`}
+        priority
+      />
+      <div className="w-1/3 rounded-lg p-6 text-center shadow-md">
+        <h1 className="mb-4 text-3xl font-bold">Not Found</h1>
+        <p className="text-destructive">Could not find requested page</p>
+        <Button variant="outline" className="mt-4 ml-2" asChild>
+          <Link href="/"> Back To Home</Link>
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default NotFoundPage
