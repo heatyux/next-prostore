104500a22e8d14998b62f2d57dc8b3c86b4fde8a
Lux <1171484646@qq.com>
2025-04-22 11:31:43 +0800

    List sample products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,13 @@
+import ProductList from '@/components/shared/product/product-list'
+import sampleData from '@/db/sample-data'
+
 const HomePage = () => {
-  return <>Prostore</>
+  return (
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Product</h2>
+      <ProductList title="Newest Arrivals" data={sampleData.products} />
+    </div>
+  )
 }
 
 export default HomePage
