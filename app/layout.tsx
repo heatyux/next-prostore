82338c8148523f3389c1f2d28ab977ccef70837a
Lux <1171484646@qq.com>
2025-04-22 09:49:41 +0800

    Root layout & constants

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '@/assets/styles/globals.css'
+import { APP_DESCRIPTION, APP_NAME, SERVRE_URL } from '@/lib/contants'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Prostore',
-  description: 'A modern store built with Next.js, ShadCN, and Prisma.',
+  title: {
+    template: `%s | Prostore`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVRE_URL),
 }
 
 export default function RootLayout({
