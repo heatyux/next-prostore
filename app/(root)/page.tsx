33c5f4ce50cb2c0cd62636a4dcf09ce8bd79b807
Lux <1171484646@qq.com>
2025-04-22 14:45:58 +0800

    Small changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,10 +3,13 @@ import sampleData from '@/db/sample-data'
 
 const HomePage = () => {
   return (
-    <div className="space-y-8">
-      <h2 className="h2-bold">Latest Product</h2>
-      <ProductList title="Newest Arrivals" data={sampleData.products} />
-    </div>
+    <>
+      <ProductList
+        title="Newest Arrivals"
+        data={sampleData.products}
+        limit={4}
+      />
+    </>
   )
 }
 
