05b8fc75488dbf335abaa1a2098dbcaddf9413db
Lux <1171484646@qq.com>
2025-04-25 11:40:14 +0800

    Serverless env config

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,13 +1,11 @@
 'use server'
 
-import { PrismaClient } from '@prisma/client'
+import { prisma } from '@/db/prisma'
 import { convertToPlainObject } from '../utils'
 import { LATEST_PRODUCT_LIMIT } from '../contants'
 
 // Get the latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient()
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCT_LIMIT,
     orderBy: {
