9b635c6018cf7cf2e3ce3f5c160366fbbcb2fead
Lux <1171484646@qq.com>
2025-04-27 16:30:04 +0800

    Sign in, sign out action and page and auth layout

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,34 @@
+'use server'
+
+import { signIn } from '@/auth'
+import { signInFormSchema } from '../validator'
+import { isRedirectError } from 'next/dist/client/components/redirect-error'
+
+// Sign in the user with credentials
+export async function signInWithCredentials(
+  prevState: unknown,
+  formData: FormData,
+) {
+  try {
+    const user = signInFormSchema.parse({
+      email: formData.get('email'),
+      password: formData.get('password'),
+    })
+
+    await signIn('credentials', user)
+
+    return {
+      success: true,
+      message: 'Signed in successfully',
+    }
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error
+    }
+  }
+}
+
+// Sign the user out
+export async function signOut() {
+  await signOut()
+}
