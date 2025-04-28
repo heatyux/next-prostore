e5b6b820ecb85d4e9faf9cddb681cdc213ab543c
Lux <1171484646@qq.com>
2025-04-28 11:49:31 +0800

    Sign up zod schema and action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,8 +1,10 @@
 'use server'
 
 import { signIn, signOut } from '@/auth'
-import { signInFormSchema } from '../validator'
+import { signInFormSchema, signUpFormSchema } from '../validator'
 import { isRedirectError } from 'next/dist/client/components/redirect-error'
+import { hashSync } from 'bcrypt-ts-edge'
+import { prisma } from '@/db/prisma'
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -35,4 +37,47 @@ export async function signInWithCredentials(
 // Sign the user out
 export async function signOutUser() {
   await signOut()
+}
+
+// Sign up user
+export async function signUp(prevstate: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get('name'),
+      email: formData.get('email'),
+      password: formData.get('password'),
+      confirmPassword: formData.get('confirmPassword'),
+    })
+
+    const plainPassword = user.password
+
+    user.password = hashSync(user.password, 10)
+
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    })
+
+    await signIn('credentials', {
+      email: user.email,
+      password: plainPassword,
+    })
+
+    return {
+      success: true,
+      message: 'User registered successfully',
+    }
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error
+    }
+
+    return {
+      success: false,
+      message: 'User was not registered',
+    }
+  }
 }
