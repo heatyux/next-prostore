334a9c6b21d2dc05e68ed64a83e06c86361af772
Lux <1171484646@qq.com>
2025-05-09 12:24:02 +0800

    Cart table and subtotal card

---
+++ app/(root)/cart/cart-table.tsx
@@ -0,0 +1,151 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { Card, CardContent } from '@/components/ui/card'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions'
+import { formatCurrency } from '@/lib/utils'
+import { Cart } from '@/types'
+import { ArrowRight, Loader, Minus, Plus } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { useTransition } from 'react'
+import { toast } from 'sonner'
+
+type CartTableProps = {
+  cart?: Cart
+}
+
+const CartTable = ({ cart }: CartTableProps) => {
+  const router = useRouter()
+  const [isPending, startTransition] = useTransition()
+
+  return (
+    <>
+      <h1 className="h2-bold py-4">Shopping Cart</h1>
+      {!cart || cart.items.length === 0 ? (
+        <div>
+          Cart is empty. <Link href="/">Go shopping</Link>
+        </div>
+      ) : (
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Item</TableHead>
+                  <TableHead className="text-center">Quantity</TableHead>
+                  <TableHead className="text-center">Price</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {cart.items.map((cartItem) => (
+                  <TableRow key={cartItem.slug}>
+                    <TableCell>
+                      <Link
+                        href={`/product/${cartItem.slug}`}
+                        className="flex items-center"
+                      >
+                        <Image
+                          src={cartItem.image}
+                          alt={cartItem.image}
+                          width={50}
+                          height={50}
+                        />
+                        <span className="px-2">{cartItem.name}</span>
+                      </Link>
+                    </TableCell>
+                    <TableCell className="flex-center gap-2">
+                      <Button
+                        disabled={isPending}
+                        variant="outline"
+                        onClick={() =>
+                          startTransition(async () => {
+                            const res = await removeItemFromCart(
+                              cartItem.productId,
+                            )
+
+                            if (!res.success) {
+                              toast.error(res.message)
+                            }
+                          })
+                        }
+                      >
+                        {isPending ? (
+                          <Loader className="h-4 w-4 animate-spin" />
+                        ) : (
+                          <Minus className="h-4 w-4" />
+                        )}
+                      </Button>
+                      <span>{cartItem.qty}</span>
+                      <Button
+                        disabled={isPending}
+                        variant="outline"
+                        onClick={() =>
+                          startTransition(async () => {
+                            const res = await addItemToCart(cartItem)
+
+                            if (!res.success) {
+                              toast.error(res.message)
+                            }
+                          })
+                        }
+                      >
+                        {isPending ? (
+                          <Loader className="h-4 w-4 animate-spin" />
+                        ) : (
+                          <Plus className="h-4 w-4" />
+                        )}
+                      </Button>
+                    </TableCell>
+                    <TableCell className="text-right">
+                      ${cartItem.price}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+
+          <Card>
+            <CardContent className="gap-4 p-4">
+              <div className="pb-3 text-xl">
+                Subtotal ({cart.items.reduce((a, c) => a + c.qty, 0)}):
+                <span className="font-bold">
+                  {formatCurrency(cart.itemsPrice)}
+                </span>
+              </div>
+              <Button
+                disabled={isPending}
+                variant="outline"
+                className="w-full"
+                onClick={() =>
+                  startTransition(() => {
+                    router.push('/shipping-address')
+                  })
+                }
+              >
+                {isPending ? (
+                  <Loader className="h-4 w-4 animate-spin" />
+                ) : (
+                  <ArrowRight className="h-4 w-4" />
+                )}{' '}
+                Proceed to Checkout
+              </Button>
+            </CardContent>
+          </Card>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default CartTable
