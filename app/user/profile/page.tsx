82a54ad127f8886417f9dc7238a80bd59056a6c4
Lux <1171484646@qq.com>
2025-05-20 15:06:03 +0800

    Update profile form

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,5 +1,23 @@
-const ProfilePage = () => {
-  return <>Profile Page</>
+import { auth } from '@/auth'
+import { Metadata } from 'next'
+import { SessionProvider } from 'next-auth/react'
+import ProfileForm from './profile-form'
+
+export const metadata: Metadata = {
+  title: 'Customer Profile',
+}
+
+const ProfilePage = async () => {
+  const session = await auth()
+
+  return (
+    <SessionProvider session={session}>
+      <div className="mx-auto max-w-md space-y-4">
+        <h2 className="h2-bold">Profile</h2>
+        <ProfileForm />
+      </div>
+    </SessionProvider>
+  )
 }
 
 export default ProfilePage
