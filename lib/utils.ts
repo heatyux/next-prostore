ea15abfc22e27bb78fb4c2a7bdbd8c4ba14f4356
Lux <1171484646@qq.com>
2025-05-23 14:47:55 +0800

    Admin overview data & chart

--- lib/utils.ts
+++ lib/utils.ts
@@ -149,3 +149,9 @@ export const formUrlQuery = ({
     },
   )
 }
+
+// Format Number
+const NUMBER_FORMATTER = new Intl.NumberFormat('en-US')
+export function formatNumber(number: number) {
+  return NUMBER_FORMATTER.format(number)
+}
