e7c65ca1be3604dce0b4e645bc083ea995921645
heatyux <421743974@qq.com>
2025-08-07 14:48:36 +0800

    Product update form

---
+++ app/admin/products/[id]/page.tsx
@@ -0,0 +1,33 @@
+import ProductForm from '@/components/shared/admin/product-form'
+import { getProductById } from '@/lib/actions/product.actions'
+import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
+
+export const metadata: Metadata = {
+  title: 'Update Product',
+}
+
+interface ProductIdPageProps {
+  params: Promise<{
+    id: string
+  }>
+}
+
+const ProductIdPage = async ({ params }: ProductIdPageProps) => {
+  const { id } = await params
+
+  const product = await getProductById(id)
+
+  if (!product) {
+    return notFound()
+  }
+
+  return (
+    <div className="mx-auto max-w-5xl space-y-8">
+      <h1 className="h2-bold">Update Product</h1>
+      <ProductForm type="Update" product={product} productId={product.id} />
+    </div>
+  )
+}
+
+export default ProductIdPage
